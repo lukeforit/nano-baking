20e4209b6aef496d4a13c0252bc3e6c115be57d3
ukasz1412 <ukasz1412@gmail.com>
2018-04-11 22:02:46 +0200

    Select recipe - loading image background

---
+++ app/src/main/java/com/rabbit/green/baking/app/recipes/BaseViewModel.java
@@ -0,0 +1,22 @@
+package com.rabbit.green.baking.app.recipes;
+
+import android.databinding.BindingAdapter;
+import android.text.TextUtils;
+import android.widget.ImageView;
+
+import com.rabbit.green.baking.app.R;
+import com.squareup.picasso.Picasso;
+
+public class BaseViewModel {
+    @BindingAdapter("imageUrl")
+    public static void setImageUrl(ImageView imageView, String imageUrl) {
+        if (TextUtils.isEmpty(imageUrl)) {
+            imageView.setImageResource(R.drawable.recipe_background);
+        } else {
+            Picasso.get().load(imageUrl)
+                    .error(R.drawable.recipe_background)
+                    .placeholder(R.drawable.recipe_background)
+                    .into(imageView);
+        }
+    }
+}
