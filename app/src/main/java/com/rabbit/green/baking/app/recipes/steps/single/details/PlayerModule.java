a329745887e2a242d2a6ad78264964107b647d0c
ukasz1412 <ukasz1412@gmail.com>
2018-05-19 18:00:52 +0200

    ExoPlayer - adding MediaSession and callbacks

--- app/src/main/java/com/rabbit/green/baking/app/recipes/steps/single/details/PlayerModule.java
+++ app/src/main/java/com/rabbit/green/baking/app/recipes/steps/single/details/PlayerModule.java
@@ -1,6 +1,8 @@
 package com.rabbit.green.baking.app.recipes.steps.single.details;
 
 import android.content.Context;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -15,10 +17,12 @@ import dagger.Module;
 import dagger.Provides;
 
 //TODO apply scope
-//TODO optional - add ExoPlayer.EventListener and MediaSession
 //TODO MediaStyle notification - just to present the idea
 @Module
 public class PlayerModule {
+
+    private static final String TAG = PlayerModule.class.getName() + "-MediaSession";
+
     @Provides
     public ExoPlayer provideExoPlayer(Context context) {
         return ExoPlayerFactory.newSimpleInstance(context,
@@ -29,5 +33,23 @@ public class PlayerModule {
     @Provides
     public DataSource.Factory provideDataSourceFactory(Context context) {
         return new DefaultDataSourceFactory(context, context.getString(R.string.app_name));
+    }
+
+    @Provides
+    public PlaybackStateCompat.Builder providePlaybackStateCompatBuilder() {
+        return new PlaybackStateCompat.Builder()
+                .setActions(PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PAUSE
+                        | PlaybackStateCompat.ACTION_PLAY_PAUSE);
+    }
+
+    @Provides
+    public MediaSessionCompat provideMediaSessionCompat(Context context,
+                                                        PlaybackStateCompat.Builder builder) {
+        MediaSessionCompat mediaSessionCompat = new MediaSessionCompat(context, TAG);
+        mediaSessionCompat.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS
+                | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+        mediaSessionCompat.setMediaButtonReceiver(null);
+        mediaSessionCompat.setPlaybackState(builder.build());
+        return mediaSessionCompat;
     }
 }
--- app/src/main/java/com/rabbit/green/baking/app/recipes/steps/single/details/StepDetailsViewModel.java
+++ app/src/main/java/com/rabbit/green/baking/app/recipes/steps/single/details/StepDetailsViewModel.java
@@ -2,10 +2,13 @@ package com.rabbit.green.baking.app.recipes.steps.single.details;
 
 import android.content.Context;
 import android.net.Uri;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.text.TextUtils;
 import android.view.View;
 
 import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -27,6 +30,14 @@ public class StepDetailsViewModel extends BaseViewModel {
     @Inject
     Context context;
 
+    @SuppressWarnings("WeakerAccess")
+    @Inject
+    MediaSessionCompat mediaSession;
+
+    @SuppressWarnings("WeakerAccess")
+    @Inject
+    PlaybackStateCompat.Builder playbackStateBuilder;
+
     private Step step;
     private long seekPosition;
 
@@ -47,6 +58,38 @@ public class StepDetailsViewModel extends BaseViewModel {
         if (seekPosition > 0) {
             exoPlayer.seekTo(seekPosition);
         }
+        mediaSession.setCallback(new MediaSessionCompat.Callback() {
+            @Override
+            public void onPause() {
+                exoPlayer.setPlayWhenReady(false);
+            }
+
+            @Override
+            public void onPlay() {
+                exoPlayer.setPlayWhenReady(true);
+            }
+
+            @Override
+            public void onStop() {
+                exoPlayer.setPlayWhenReady(false);
+                exoPlayer.seekTo(0);
+            }
+        });
+        mediaSession.setActive(true);
+
+        exoPlayer.addListener(new Player.DefaultEventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                if (playWhenReady && playbackState == Player.STATE_READY) {
+                    playbackStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                            exoPlayer.getCurrentPosition(), 1f);
+                } else if (playbackState == Player.STATE_READY) {
+                    playbackStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                            exoPlayer.getCurrentPosition(), 1f);
+                }
+                mediaSession.setPlaybackState(playbackStateBuilder.build());
+            }
+        });
     }
 
     public long getPlayerCurrentPosition() {
@@ -58,6 +101,7 @@ public class StepDetailsViewModel extends BaseViewModel {
     }
 
     public void releasePlayer() {
+        mediaSession.setActive(false);
         exoPlayer.release();
     }
 
