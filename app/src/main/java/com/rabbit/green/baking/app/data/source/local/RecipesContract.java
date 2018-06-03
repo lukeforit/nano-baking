6da8b7c8c4733247f956fc6753ab77e2a3cc0416
ukasz1412 <ukasz1412@gmail.com>
2018-06-03 17:49:23 +0200

    Content provider - db contract

---
+++ app/src/main/java/com/rabbit/green/baking/app/data/source/local/RecipesContract.java
@@ -0,0 +1,44 @@
+package com.rabbit.green.baking.app.data.source.local;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class RecipesContract {
+    private static final String SCHEME = "content://";
+    public static final String AUTHORITY = "com.rabbit.green.baking";
+    static final String PATH_RECIPE = "recipe";
+    private static final Uri BASE_CONTENT_URI = Uri.parse(SCHEME + AUTHORITY);
+
+    static final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + RecipeEntry.TABLE_NAME + "(" +
+            BaseColumns._ID + " INTEGER PRIMARY KEY, " +
+            RecipeEntry.COLUMN_NAME + " TEXT NOT NULL " +
+            ");";
+
+    static final String SQL_CREATE_INGREDIENT_TABLE = "CREATE TABLE " + IngredientEntry.TABLE_NAME + "(" +
+            BaseColumns._ID + " INTEGER PRIMARY KEY, " +
+            IngredientEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+            IngredientEntry.COLUMN_MEASURE + " TEXT NOT NULL, " +
+            IngredientEntry.COLUMN_QTY + " REAL NOT NULL, " +
+            IngredientEntry.COLUMN_RECIPE_ID + " TEXT NOT NULL " +
+            ");";
+
+    static final Uri CONTENT_URI = BASE_CONTENT_URI
+            .buildUpon()
+            .appendPath(PATH_RECIPE)
+            .build();
+
+    static class RecipeEntry implements BaseColumns {
+
+        static final String TABLE_NAME = "recipe";
+        static final String COLUMN_NAME = "name";
+    }
+
+    static class IngredientEntry implements BaseColumns {
+
+        static final String TABLE_NAME = "ingredient";
+        static final String COLUMN_NAME = "name";
+        static final String COLUMN_MEASURE = "measure";
+        static final String COLUMN_QTY = "qty";
+        static final String COLUMN_RECIPE_ID = "recipeId";
+    }
+}
